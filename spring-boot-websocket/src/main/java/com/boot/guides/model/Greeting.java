03ac1cfe06cebf7a72d9b07d450b32180b53e390
ysgo <wjion8428@gmail.com>
2020-04-12 22:51:20 +0900

    create websocket starter basic
    기본적인 메시지 전송 형태만 구현

---
+++ spring-boot-websocket/src/main/java/com/boot/guides/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.boot.guides.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+	private String content;
+}
